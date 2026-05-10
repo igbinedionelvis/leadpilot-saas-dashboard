b0e1816a74aeb38ed4a65e6ba4483dfb327a4ad3
igbinedionelvis <eligbinosa@gmail.com>
2026-05-10 12:22:31 +0100

    Update dashboard user profile info

--- src/app/store/auth-store.ts
+++ src/app/store/auth-store.ts
@@ -1,15 +1,15 @@
-import { create } from 'zustand'
+import { create } from "zustand";
 
 type AuthState = {
-  isAuthenticated: boolean
-  userName: string
-  signIn: () => void
-  signOut: () => void
-}
+  isAuthenticated: boolean;
+  userName: string;
+  signIn: () => void;
+  signOut: () => void;
+};
 
 export const useAuthStore = create<AuthState>((set) => ({
   isAuthenticated: true,
-  userName: 'Ben Wilson',
+  userName: "Elvis Igbinedion",
   signIn: () => set({ isAuthenticated: true }),
   signOut: () => set({ isAuthenticated: false }),
-}))
+}));
